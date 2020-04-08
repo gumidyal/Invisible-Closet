1acea64f5e980dbbee14a570996975d1b715ba1d
Griffin Mareske <gmareske@gmail.com>
2020-04-08 08:56:57 -0500

    Add example pages to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  get 'pages/home', as: 'home'
+
+  get 'pages/needs', as: 'needs'
+
+  get 'pages/request', to: 'pages#request_items', as: 'request_items'
+
+  get 'pages/inventory', as: 'inventory'
+
+  get 'pages/admin', as: 'admin'
+
+  get 'pages/donate', as: 'donate'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
