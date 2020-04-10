4a4cb4120a340dcb3a111bbaca15cf1c92571ddb
sushengy <sushengy@grinnell.edu>
2020-04-09 21:39:43 -0500

    add donation, search and home

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def donation
+  end
+
+  def search
+  end
+
+  def about
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/donation'
+
+  get 'static_pages/search'
+
+  get 'static_pages/about'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
