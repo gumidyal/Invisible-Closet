cfb87c753c929d0d597780d2bee94152c033f037
Jared <boothjar@grinnell.edu>
2020-04-24 00:08:09 -0500

    model update

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,5 +1,5 @@
 class Donation < ApplicationRecord
-    validates :items, presence: true, length: { maximum: 500}
+    validates :description, presence: true, length: { maximum: 500}
     validates :name, length: {maximum: 30}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, length: { maximum: 255 },
